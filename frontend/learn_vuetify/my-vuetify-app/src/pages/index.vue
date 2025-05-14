c639ed1299289c422160332583ec14dd6f82a7a2
RohimiRaslam <mrohimi@gmail.com>
2025-05-14 12:00:32 +0800

    created vuetify project

---
+++ frontend/learn_vuetify/my-vuetify-app/src/pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <!-- <div class="text-center pt-3">centered with padding</div>
+  <div class="bg-primary opacity-40 pa-10 text-white text-center pa-3">
+    Primary Background
+  </div>
+  <v-container>
+    <v-row align="center" justify="space-between">
+      <v-col class="bg-primary" cols="4" md="6"
+        >Half-width on medium screens</v-col
+      >
+      <v-col class="bg-primary" cols="4" md="6">test</v-col>
+    </v-row>
+  </v-container>
+
+  <div class="text-h1">test</div>
+  <div class="text-h2">test</div>
+  <div class="text-h3">test</div>
+  <div class="text-high-emphasis">test</div>
+
+  <div class="w-50 h-25 bg-secondary text-capitalize text-center">
+    custom dimension
+  </div>
+
+  <v-row align="center" justify="space-between">
+    <v-cols class="bg-primary" cols="4">aligned left</v-cols>
+    <v-cols class="bg-primary" cols="4">aligned center</v-cols>
+    <v-cols class="bg-primary" cols="4">aligned right</v-cols>
+  </v-row>
+
+  <div class="elevation-10 bg-blue pa-4 text-capitalize">
+    shadow and background
+  </div>
+
+  <div class="rounded-lg border-double pa-2">Border and styling</div>
+
+  <div class="opacity-40">blur and opacity</div> -->
+
+  <div class="text-h1 text-center">test</div>
+
+  <div class="d-flex justify-space-around">
+    <v-card width="300px" cols="6">
+      <v-card-title class="text-h6 text-md-h5 text-lg-h4">Title</v-card-title>
+      <v-card-text> Body text </v-card-text>
+    </v-card>
+
+    <v-card width="300px" cols="6">
+      <v-card-title class="text-h6 text-md-h5 text-lg-h4">Title</v-card-title>
+      <v-card-text> Body text </v-card-text>
+    </v-card>
+  </div>
+
+  <div class="bg-primary text-center pa-3">centered with padding</div>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
