16eb48b2b0c0e152c53c4f3098342d9253f1d541
RohimiRaslam <mrohimi@gmail.com>
2025-05-15 13:25:49 +0800

    added and modified lines for new lessons

--- frontend/learn_vuetify/my-vuetify-app/src/pages/index.vue
+++ frontend/learn_vuetify/my-vuetify-app/src/pages/index.vue
@@ -36,6 +36,9 @@
 
   <div class="opacity-40">blur and opacity</div> -->
 
+  <!-- ============================================================= -->
+  <!-- =======================EXERCISE START========================
+       =============================================================
   <div class="text-h1 text-center">test</div>
 
   <div class="d-flex justify-space-around">
@@ -50,9 +53,225 @@
     </v-card>
   </div>
 
-  <div class="bg-primary text-center pa-3">centered with padding</div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-sheet class="pa-2 ma-2 bg-primary text-center">testing</v-sheet>
+      </v-col>
+    </v-row>
+  </v-container> 
+  =============================================================
+  =======================EXERCISE END==========================
+  =============================================================-->
+
+  <!-- <v-app>
+    <v-container class="pa-4">
+      <v-btn
+        rounded="md"
+        color="yellow-darken-4"
+        class="text-black"
+        @click="() => console.log('button clicked')"
+        >test button
+      </v-btn>
+
+      <v-dialog>
+        <v-card>
+          <v-card-title>dialog title</v-card-title>
+          <v-card-text>dialog content goes here</v-card-text>
+          <v-card-action>
+            <v-btn text @click="dialog = false">close</v-btn>
+          </v-card-action>
+        </v-card>
+      </v-dialog>
+    </v-container>
+  </v-app>-->
+
+  <!-- <v-container class="pa-4">
+    <v-btn
+      rounded="md"
+      color="yellow-darken-4"
+      class="text-black"
+      @click="dialog = true"
+      >open dialog
+    </v-btn>
+
+    <v-dialog v-model="dialog" max-width="500">
+      <v-card>
+        <v-card-title>dialog title</v-card-title>
+        <v-card-text>dialog content goes here</v-card-text>
+        <v-card-action>
+          <v-btn text @click="dialog = false">close</v-btn>
+        </v-card-action>
+      </v-card>
+    </v-dialog>
+
+    <div class="d-none d-md-block">
+      visible only on medium screens and larger
+    </div>
+
+    <div>
+      <h1 class="d-none d-sm-blockn">
+        this heading is visible on small screen and above
+      </h1>
+      <p class="d-flex d-sm-block">
+        this paragraph is displayed on small screeens and as a block on larger
+        screens
+      </p>
+    </div> -->
+
+  <!-- <v-row class="ma-5">
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        class="border-lg"
+        align="center"
+        justify="center"
+      >
+        <div>Column 1</div>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        class="border-lg"
+        align="center"
+        justify="center"
+      >
+        <div>Column 2</div>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        class="border-lg"
+        align="center"
+        justify="center"
+      >
+        <div>Column 3</div>
+      </v-col>
+    </v-row> -->
+
+  <!-- <v-row>
+      <v-col>
+        <v-card
+          flat
+          cols="12"
+          sm="6"
+          md="4"
+          class="border-lg"
+          align="center"
+          justify="center"
+          title="Column 1"
+        ></v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          flat
+          cols="12"
+          sm="6"
+          md="4"
+          class="border-lg"
+          align="center"
+          justify="center"
+          title="Column 2"
+        ></v-card>
+      </v-col>
+      <v-col>
+        <v-card
+          flat
+          cols="12 "
+          sm="6"
+          md="4"
+          class="border-lg"
+          align="center"
+          justify="center"
+          title="Column 3"
+        ></v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-row>
+    <v-col align="center">
+      <div clas="text-center">
+        <v-menu transition="fade-transition">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              flat
+              class="text-black"
+              color="yellow-darken-4"
+              v-bind="props"
+              >click for fade transition</v-btn
+            >
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title v-text="'hello'"></v-list-item-title>
+              <v-list-item-title v-text="'world'"></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col>
+      <v-hover>
+        <template v-slot:default="{ isHovering, props }">
+          <v-card
+            flat
+            class="ml-10 mr-10 text-black"
+            v-bind="props"
+            :color="isHovering ? 'primary' : 'secondary'"
+            title="Hover over me"
+            text="Hello World"
+          ></v-card>
+        </template>
+      </v-hover>
+    </v-col>
+  </v-row>
+
+  <v-row>
+    <v-col align="start">
+      <v-btn flat class="custom-btn">custom button</v-btn>
+    </v-col>
+    <v-col>
+      <v-btn flat class="custom-btn" @click="toggleTheme">toggle theme</v-btn>
+    </v-col>
+  </v-row> -->
+
+  <v-btn class="ma-5" v-tooltip="'Tooltip'">Hover me</v-btn>
+  <div class="d-flex justify-center ga-4 py-10">
+    <v-btn v-tooltip:start="'Tooltip at the start'">start</v-btn>
+    <v-btn v-tooltip:end="'Tooltip at the end'">end</v-btn>
+    <v-btn v-tooltip:top="'Tooltip at the top'">top</v-btn>
+    <v-btn v-tooltip:bottom="'Tooltip at the bottom'">bottom</v-btn>
+    <v-btn v-tooltip:bottom-end="'Tooltip at the bottom-end'">bottom-end</v-btn>
+  </div>
 </template>
 
-<script lang="ts" setup>
-//
+<style>
+/* .custom-btn {
+  background-color: red;
+  color: white;
+  border-radius: 8px;
+} */
+</style>
+
+<!-- lang="ts" setup -->
+<script>
+// import { useTheme } from "vuetify";
+// const theme = useTheme();
+// function toggleTHeme() {
+//   theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+// }
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
 </script>
